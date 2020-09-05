5df68eca206ff7dd3be1c658deb9c6075d6e30a4
Paulmicha <paul.michalet@gmail.com>
2020-09-05 14:34:38 -0300

    feat(test): improve local HTML test files URLs

--- test/Page.test.js
+++ test/Page.test.js
@@ -4,13 +4,14 @@ const path = require('path')
 const Page = require('../src/Page')
 const defaultConfig = require('../src/utils/default_config.js')
 
-// We use a custom object to mock '../src/Main' class.
+// We use a custom object to mock '../src/Main' class, which is not the one
+// being tested here.
 const ScraperMock = {}
 ScraperMock.getSetting = (setting) => defaultConfig.setting(setting)
 
-// Local static pages URLs (file:///).
+// Local static pages URLs.
 const urls = {
-  blog: path.resolve(__dirname, 'static', 'Blog.html')
+  blog: 'file://' + path.resolve(__dirname, 'static', 'Blog.html')
 }
 
 // This runs before all tests.
@@ -21,6 +22,29 @@ test.before(async t => {
 test('1. Opening a URL', async t => {
   const pageWorker = new Page(ScraperMock)
   await pageWorker.init()
-  await pageWorker.page.goto(`file:${urls.blog}`)
-  t.is(`file://${urls.blog}`, pageWorker.page.url())
+  // await pageWorker.page.goto(`file:${urls.blog}`)
+  // t.is(`file://${urls.blog}`, pageWorker.page.url())
+  await pageWorker.page.goto(urls.blog)
+  t.is(urls.blog, pageWorker.page.url())
 })
+
+// TODO test common crawling issues :
+//  - Position:fixed (test if this does not make screenshots partially
+//    unreadable, and if so, convert to absolute positions ?)
+//  - Timeouts (+ closing modals, popups) :
+//    cf. https://stackoverflow.com/a/48865555/2592338
+//    cf. https://github.com/bargoldi/time-events-manager
+//  - Infinite scroll :
+//    cf. https://intoli.com/blog/scrape-infinite-scroll/
+//    example :
+//      let items = [];
+//      try {
+//        let previousHeight;
+//        while (items.length < itemTargetCount) {
+//          items = await page.evaluate(extractItems);
+//          previousHeight = await page.evaluate('document.body.scrollHeight');
+//          await page.evaluate('window.scrollTo(0, document.body.scrollHeight)');
+//          await page.waitForFunction(`document.body.scrollHeight > ${previousHeight}`);
+//          await page.waitFor(scrollDelay);
+//        }
+//      } catch(e) {}
